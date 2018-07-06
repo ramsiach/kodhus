989ff83712c46bc98d9a4de12f2dc932426ad9e8
RaminSiahcheshmi <ramin.siahcheshmi@meltwater.com>
2018-07-07 00:15:04 +0200

    modifications and fixing styles

--- src/js/kodhus.js
+++ src/js/kodhus.js
@@ -71,108 +71,119 @@ mobileTrigger.addEventListener('click', function() {
 
 
 
-var carousel = document.querySelector('.cdt-carousel .wrapper');
-var active = document.querySelector('.cdt-carousel .active');
-var activeIndex = -1;
-const length = carousel.childElementCount;
-const carouselItems = document.querySelectorAll('.cdt-carousel .wrapper div')
-const getActiveElementIndex = function() {
-    carouselItems.forEach(function(el, i) {
-        if (el.classList.contains('active'))
-            activeIndex = i;
-    });
-}
-getActiveElementIndex();
-const carouselPrevBtn = document.querySelector('.cdt-carousel .prev-btn');
-const carouselNextBtn = document.querySelector('.cdt-carousel .next-btn');
-var animating = false;
-carouselPrevBtn.addEventListener('click', function() {
-    if (animating) return;
-    var current = carousel.children[activeIndex];
-    var prevIndex = (activeIndex === 0) ? (length - 1) : activeIndex - 1;
-    var next = carousel.children[prevIndex];
-    animating = true;
-    setTimeout(function() {
-        current.classList.add('right');
-    }, 0);
-    current.addEventListener("transitionend", function(event) {
-        current.classList.remove('active');
-        current.classList.remove('right');
-    });
-    next.classList.add('prev');
-    setTimeout(function() {
-        next.classList.add('right');
-    }, 0);
-    next.addEventListener('transitionend', function(event) {
-        next.classList.add('active');
-        next.classList.remove('prev');
-        next.classList.remove('right');
-        activeIndex = prevIndex;
-        animating = false;
-    });
-});
+// var carousel = document.querySelector('.cdt-carousel .wrapper');
+// var active = document.querySelector('.cdt-carousel .active');
+// var activeIndex = -1;
+// let length = 0;
+// let carouselPrevBtn, carouselNextBtn;
+// let current, next;
+// if (carousel) {
+//     length = carousel.childElementCount;
+//     const carouselItems = document.querySelectorAll('.cdt-carousel .wrapper div')
+//     const getActiveElementIndex = function() {
+//         carouselItems.forEach(function(el, i) {
+//             if (el.classList.contains('active'))
+//                 activeIndex = i;
+//         });
+//     }
+//     getActiveElementIndex();
+//     carouselPrevBtn = document.querySelector('.cdt-carousel .prev-btn');
+//     carouselNextBtn = document.querySelector('.cdt-carousel .next-btn');
+//     var animating = false;
+//     carouselPrevBtn.addEventListener('click', function() {
+//         if (animating) return;
+//         current = carousel.children[activeIndex];
+//         var prevIndex = (activeIndex === 0) ? (length - 1) : activeIndex - 1;
+//         next = carousel.children[prevIndex];
+//         animating = true;
+//         setTimeout(function() {
+//             current.classList.add('right');
+//         }, 0);
+//         current.addEventListener("transitionend", function(event) {
+//             current.classList.remove('active');
+//             current.classList.remove('right');
+//         });
+//         next.classList.add('prev');
+//         setTimeout(function() {
+//             next.classList.add('right');
+//         }, 0);
+//         next.addEventListener('transitionend', function(event) {
+//             next.classList.add('active');
+//             next.classList.remove('prev');
+//             next.classList.remove('right');
+//             activeIndex = prevIndex;
+//             animating = false;
+//         });
+//     });
+// }
 
-carouselNextBtn.addEventListener('click', function() {
-    if (animating) return;
-    var current = carousel.children[activeIndex];
-    //current.classList.add('left');
-    var nextIndex = (activeIndex === length - 1) ? 0 : activeIndex + 1;
-    var next = carousel.children[nextIndex];
-    current.classList.add('prev');
-    next.classList.add('next');
+// carouselNextBtn.addEventListener('click', function() {
+//     if (animating) return;
+//     var current = carousel.children[activeIndex];
+//     //current.classList.add('left');
+//     var nextIndex = (activeIndex === length - 1) ? 0 : activeIndex + 1;
+//     var next = carousel.children[nextIndex];
+//     current.classList.add('prev');
+//     next.classList.add('next');
 
-    setTimeout(function() {
-        animating = true;
-        current.classList.add('left');
-        next.classList.add('left');
-    }, 0);
-    current.addEventListener("transitionend", function(event) {
-        next.classList.remove('next');
-        next.classList.remove('left');
-        next.classList.add('active');
-        current.classList.remove('prev');
-        current.classList.remove('left');
-        current.classList.remove('active');
-        activeIndex = nextIndex;
-        animating = false;
-    });
-});
+//     setTimeout(function() {
+//         animating = true;
+//         current.classList.add('left');
+//         next.classList.add('left');
+//     }, 0);
+//     current.addEventListener("transitionend", function(event) {
+//         next.classList.remove('next');
+//         next.classList.remove('left');
+//         next.classList.add('active');
+//         current.classList.remove('prev');
+//         current.classList.remove('left');
+//         current.classList.remove('active');
+//         activeIndex = nextIndex;
+//         animating = false;
+//     });
+// });
 
-// Reveal on scroll
-var last_known_scroll_position = 0;
-var ticking = false;
+// // Reveal on scroll
+// var last_known_scroll_position = 0;
+// var ticking = false;
 
-const element = document.querySelector('[data-aiv]');
-const elementHeight = element.offsetHeight;
-const elementTop = element.offsetTop;
-element.style.transformOrigin = "50% 100%";
-element.style.transform = "translateY(" + 100 + "px) scale(0.9)";
-element.style.opacity = "0";
+// const element = document.querySelector('[data-aiv]');
+// const elementHeight = element.offsetHeight;
+// const elementTop = element.offsetTop;
+// element.style.transformOrigin = "50% 100%";
+// element.style.transform = "translateY(" + 100 + "px) scale(0.9)";
+// element.style.opacity = "0";
 
-function doSomething(scroll_pos) {
-    console.log(scroll_pos + window.innerHeight, elementTop + elementHeight);
-    if (scroll_pos + window.innerHeight > elementTop + elementHeight * .4) {
-        element.style.transition = "all 1s cubic-bezier(0.6, 0.2, 0.1, 1)";
-        setTimeout(function() {
-            element.style.transform = "translateY(0) scale(1)";
-            element.style.opacity = "1";
-        }, 0);
-    }
-}
+// function doSomething(scroll_pos) {
+//     console.log(scroll_pos + window.innerHeight, elementTop + elementHeight);
+//     if (scroll_pos + window.innerHeight > elementTop + elementHeight * .4) {
+//         element.style.transition = "all 1s cubic-bezier(0.6, 0.2, 0.1, 1)";
+//         setTimeout(function() {
+//             element.style.transform = "translateY(0) scale(1)";
+//             element.style.opacity = "1";
+//         }, 0);
+//     }
+// }
 
-window.addEventListener('scroll', function(e) {
-    console.log('called');
-    last_known_scroll_position = window.scrollY;
-    if (!ticking) {
-        window.requestAnimationFrame(function() {
-            doSomething(last_known_scroll_position);
-            ticking = false;
-        });
+// window.addEventListener('scroll', function(e) {
+//     console.log('called');
+//     last_known_scroll_position = window.scrollY;
+//     if (!ticking) {
+//         window.requestAnimationFrame(function() {
+//             doSomething(last_known_scroll_position);
+//             ticking = false;
+//         });
 
-        ticking = true;
-    }
-});
-setTimeout(function() {
-    window.dispatchEvent(new Event('scroll'));
-}, 0);
+//         ticking = true;
+//     }
+// });
+// setTimeout(function() {
+//     window.dispatchEvent(new Event('scroll'));
+// }, 0);
 // end Reveal on scroll
+
+// Code highlight
+document.querySelectorAll('pre code').forEach(function(block, i) {
+    hljs.highlightBlock(block);
+});
+  
